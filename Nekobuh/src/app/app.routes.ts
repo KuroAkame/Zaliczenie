d41fd1f48e453eeef11bcdd2d1b0d393194c376c
KuroAkame <justnouvre@gmail.com>
2025-05-19 18:07:59 +0200

    dodanie wygladuu i polaczenia login register

--- Nekobuh/src/app/app.routes.ts
+++ Nekobuh/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import {BodyComponent} from './components/body/body.component';
 import {ItemComponent} from './components/item/item.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegistersentComponent} from './components/registersent/registersent.component';
 
 export const routes: Routes = [
   {path:'',component:BodyComponent},
   {path:'item', component:ItemComponent},
+  {path: 'login', component: LoginComponent},
+  {path:'registersent', component: RegistersentComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
---
+++ Nekobuh/src/app/components/registersent/registersent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+@Component({
+  selector: 'app-registersent',
+  imports: [RouterLink],
+  templateUrl: './registersent.component.html',
+  styleUrl: './registersent.component.css'
+})
+export class RegistersentComponent {
+
+}
